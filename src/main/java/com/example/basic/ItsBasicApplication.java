51a59a17738a61b54fdad88e5dba6991fb82d355
sangwoo kim <wang9ri@gmail.com>
2022-11-17 22:48:48 +0900

    init

---
+++ src/main/java/com/example/basic/ItsBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItsBasicApplication.class, args);
+    }
+
+}
